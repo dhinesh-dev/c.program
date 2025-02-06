52a9e041cafe38378b8ebb0839e7f8dd9384fc20
dhinesh-dev <dhinesh.dev03@gmail.com>
2025-02-06 10:32:30 +0000

    added new file variable and sca functionsnf

---
+++ scanf.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+ main ()
+{
+    int x;
+    float f;
+    char ch;
+    
+
+    printf("Enter a Number : ");
+    scanf("%d",&x);
+    printf("\nEnter a Decimal Number : ");
+    scanf("%f",&f);
+    printf("\nEnter a Single Charocter : ");
+    scanf("%s",&ch);
+    
+    printf("the intiger number is %d \n the float point is %f \n the charcter is %c \n",x,f,ch);
+
+    
+    
+    
+    return 0;
+}
---
+++ veriable.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a=13;
+    char ch='d';
+    float f=12.9;
+    double d=3123;
+    printf("%d\n",a);
+     printf("%c\n",ch);
+      printf("%f\n",f);
+       printf("%lf",d);
+
+return 0;
+}
